8766478450a841ce2fdfc661219f737167bf6a8e
RamySassi <rami9b3@live.fr>
2024-01-26 13:23:12 +0100

    test2

--- js/pageAcc.js
+++ js/pageAcc.js
@@ -1,64 +1,85 @@
+var cartItems = [];
 
+function addItemToCart(itemName, itemPrice) {
+    var item = {
+        name: itemName,
+        price: itemPrice
+    };
+    cartItems.push(item);
+}
 
+function displayCart() {
+    var cartContainer = document.getElementById("cart-container");
+    cartContainer.innerHTML = "";
 
-function Article(type,imageSrc,price){
+    cartItems.forEach(function (item) {
+        var itemDiv = document.createElement("div");
+        itemDiv.innerHTML = item.name + " - $" + item.price;
+        cartContainer.appendChild(itemDiv);
+    }); }
+  
+function Article(type,imageSrc,price,){
     var obj={}
     
     obj.imageSrc=imageSrc
     obj.type=type
     obj.price=price
+
     return obj
 }
 
-var article1=Article("capush" , {src:"/image/capush1.webp"},100)
-var article2=Article("capush" , {src:"/image/capush2.webp"},150 )
-var article3=Article("capush" , {src:"/image/capush3.webp"},200)
+var article1=Article("capush" , {src:"/PerProject/image/capush1.webp"},100)
+var article2=Article("capush" , {src:"/PerProject/image/capush2.webp"},150 )
+var article3=Article("capush" , {src:"/PerProject/image/capush3.webp"},199)
 
-var article4=Article("jeans" , {src:"/image/jean 1.webp"},100)
-var article5=Article("jeans" , {src:"/image/jean 3.webp"},100)
-var article6=Article("jeans" , {src:"/image/jean 2.jpg"},100)
+var article4=Article("jeans" , {src:"/PerProject/image/jean 1.webp"},109)
+var article5=Article("jeans" , {src:"/PerProject/image/jean 3.webp"},180)
+var article6=Article("jeans" , {src:"/PerProject/image/jean 2.jpg"},'25$  !in sold!')
 
-var article7=Article("doudounes" , {src:"/image/doudounes1.webp"},100)
-var article8=Article("doudounes" , {src:"/image/doudounes2.webp"},100)
-var article9=Article("doudounes" , {src:"/image/doudounes3.webp"},100)
+var article7=Article("doudounes" , {src:"/PerProject/image/doudounes1.webp"},100)
+var article8=Article("doudounes" , {src:"/PerProject/image/doudounes2.webp"},100)
+var article9=Article("doudounes" , {src:"/PerProject/image/doudounes3.webp"},100)
 
-var article10=Article("chaussures" , {src:"/image/chaus 1.webp"},100)
-var article11=Article("chaussures" , {src:"/image/chaus 2.webp"},100)
-var article12=Article("chaussures" , {src:"/image/chaus 3.webp"},100)
+var article10=Article("chaussures" , {src:"/PerProject/image/chaus 1.webp"},100)
+var article11=Article("chaussures" , {src:"/PerProject/image/chaus 2.webp"},100)
+var article12=Article("chaussures" , {src:"/PerProject/image/chaus 3.webp"},100)
 
 
 var capush = [article1, article2, article3];
 
 for (var i = 0; i < capush.length; i++) {
     $('.capush').append(`
-        <div class="img-name" style="width: 350px;    height: 350px;">
-            <div class="img" style="width: 350px; height: 350px; background-color: white; position: relative; left: 20px; top: 20px;">
+        <div class="img-name">
+            <div class="img" >
                 <div class="capush-item">
-                    <img style="width: 350px; height: 350px;" src="${capush[i].imageSrc.src}">
-                </div>
+         <img  src="${capush[i].imageSrc.src}">
+                      </div>
                 <div class="price">
-                    ${capush[i].price}
+         ${capush[i].price}
                 </div>
+        
+ <button onclick="addItemToCart('Produit ',  ${capush[i].price} )"> add to cart</button>
+   </div>
             </div>
         </div>
     `);
 }
 
 
-
-
 var jeans=[article4,article5,article6]
 
 for (var i = 0; i < jeans.length; i++) {
     $('.jeans').append(`
-        <div class="img-name" style="width: 350px; height: 350px;">
-            <div class="img" style="width: 350px; height: 350px;  display:inline-block ; background-color: white; position: relative; left: 20px; top: 20px;">
+        <div class="img-name" >
+            <div class="img" >
                 <div class="jeans-item">
-                    <img style="width: 350px; height: 350px;" src="${jeans[i].imageSrc.src}">
+         <img  src="${jeans[i].imageSrc.src}">
                 </div>
                 <div class="price">
                     ${jeans[i].price}
                 </div>
+                <button onclick="addItemToCart('Produit 1',  ${capush[i].price} )"> add to cart</button>
+   </div>
             </div>
         </div>
     `);
@@ -69,14 +90,15 @@ var doudounes=[article7,article8,article9]
 
 for (var i = 0; i< doudounes.length; i++) {
     $('.doudounes').append(`
-        <div class="img-name" style="width: 350px; height: 350px;">
-            <div class="img" style="width: 350px; height: 350px; background-color: white; position: relative; left: 20px; top: 20px;">
-                <div class="doudounes-item">
-                    <img style="width: 350px; height: 350px;" src="${doudounes[i].imageSrc.src}">
+    <div class="img" >
+    <div class="jeans-item">
+        <img  src="${doudounes[i].imageSrc.src}">
                 </div>
-                <div class="price">
+        <div class="price">
                     ${doudounes[i].price}
                 </div>
+                <button onclick="addItemToCart('Produit 1',  ${capush[i].price} )"> add to cart</button>
+   </div>
             </div>
         </div>
     `);
@@ -84,25 +106,20 @@ for (var i = 0; i< doudounes.length; i++) {
 
 
 
-    var chaussures = [article10, article11, article12];
+var chaussures = [article10, article11, article12];
 
-    for (var i = 0; i < chaussures.length; i++) {
-        $('.chaussures').append(`
-            <div class="img-name" style="width: 350px; height: 350px;">
-                <div class="img" style="width: 350px; height: 350px; background-color: white; position: relative; left: 20px; top: 20px;">
-                    <div class="chaussures-item">
-                        <img style="width: 350px; height: 350px;" src="${chaussures[i].imageSrc.src}">
-                    </div>
-                    <div class="price">
-                        ${chaussures[i].price}
-                    </div>
-                </div>
-            </div>
-        `);
-    }
-
-
-
-
+for (var i = 0; i < chaussures.length; i++) {
+$('.chaussures').append(`
+<div class="img" >
+<div class="jeans-item">
+<img  src="${chaussures[i].imageSrc.src}">
+</div>
+<div class="price">
+${chaussures[i].price}
+</div>
+<button onclick="addItemToCart('Produit 1',  ${capush[i].price} )"> add to cart</button>
+   </div>
+</div></div> `);
+ }
 
 
